88369f1fbeb4af861a19cf856e7a51c672caf750
astmoh5 <118526519+astmoh5@users.noreply.github.com>
2024-07-13 21:59:15 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,39 @@
-import matplotlib.image as img
+from PIL import Image
 import matplotlib.pyplot as plt
+import numpy as np
+from random import randint, random
 
-null = (img.imread('nums/null_0.jpg'))
+img = tuple
+nums = [
+    (np.asarray(Image.open('nums/zero_0.jpg')), np.asarray(Image.open('nums/zero_1.jpg'))),
+    (np.asarray(Image.open('nums/one_0.jpg')), np.asarray(Image.open('nums/one_1.jpg')))
+    ]
 
-plt.imshow(null)
+img[1] = randint(0,1)
+img[0] = nums[img[1]][randint(0,len(nums[img[1]]))]
+l1_0, l1_1, l1_2, l1_3, l1_4, l1_5, l1_6, l1_7, l1_8, l1_9, l1_10 = random, random, random, random, random, random, random, random, random, random
+out = int
+accuracy = 50
+results = list
+[
+    #input
+    img[0],
+
+    #Layer1
+    [
+        l1_0, l1_1, l1_2, l1_3, l1_4, l1_5, l1_6, l1_7, l1_8, l1_9, l1_10
+    ],
+
+    #output     
+    out
+
+]
+
+while accuracy<90:
+
+
+
+    if out == img[1]:
+        results.append(1)
+    else: results.append(0)
+    accuracy = sum(results)/len(results)
